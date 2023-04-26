a7d1ea694e703567c71a9b3d4eff7e46addd246c
xrstf <git@xrstf.de>
2023-04-26 12:37:51 +0200

    import client-go OIDC support

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 	"k8s.io/client-go/tools/clientcmd"
 	watchtools "k8s.io/client-go/tools/watch"
 )
